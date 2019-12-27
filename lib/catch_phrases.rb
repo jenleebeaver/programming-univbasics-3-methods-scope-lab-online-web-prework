2c6815a4fad87c46ea011f63cb4b2d4222b7f35e
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-27 12:07:36 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,12 @@
+
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 
 def toadstool
-  status = "Thank you Mario! But Our Princess Is In Another Castle!\n"
   puts status
 end
